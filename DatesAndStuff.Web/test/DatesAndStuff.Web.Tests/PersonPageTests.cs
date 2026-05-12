fdd12dd690ecf1d21ce6da5c4cab8fd9f1d3dced
papnoemi199999 <papnoemi199999@gmail.com>
2026-05-12 22:16:29 +0300

    fix test

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -154,7 +154,7 @@ public class PersonPageTests
         var input = wait.Until(ExpectedConditions.ElementToBeClickable(inputBy));
         input.Click();
         input.Clear();
-        input.SendKeys("-10");
+        input.SendKeys("-11");
 
         // Act
         var submitButton = wait.Until(ExpectedConditions.ElementToBeClickable(submitButtonBy));
